eefa5426c801731216ffeabca5fc047d421b4fb6
jltkd <jl@turnkeydigital.com>
2021-06-15 14:53:32 -0500

    Access to Nova on Live server

--- routes/auth.php
+++ routes/auth.php
@@ -10,9 +10,13 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisteredUserController::class, 'create'])
-                ->middleware('guest')
-                ->name('register');
+//Route::get('/register', [RegisteredUserController::class, 'create'])
+//                ->middleware('guest')
+//                ->name('register');
+
+Route::get('/register', function () {
+    return view('welcome');
+});
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest');
