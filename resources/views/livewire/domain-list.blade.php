ec289d1a3dd2cc83682cb88a1c4cb3e3d8e9257c
jltkd <jl@turnkeydigital.com>
2021-06-17 12:46:40 -0500

    Badges for manages in domain list

--- resources/views/livewire/domain-list.blade.php
+++ resources/views/livewire/domain-list.blade.php
@@ -30,7 +30,9 @@
                     @forelse($domains as $domain)
                     <tr class="{{ $loop->even ? 'bg-gray-50' : '' }}">
                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                            <a class="flex content-center" href="{{ $domain->domain }}" target="_blank">{{ $domain->name }} <x-external-link-icon/></a>
+                            <a class="flex content-center" href="{{ $domain->domain }}" target="_blank">
+                                {{ $domain->name }} <x-external-link-icon/>
+                            </a>
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                             <a class="flex content-center" href="http://{{$domain->server->ip_address}}:2086" target="_blank">{{ $domain->server->name }} <x-external-link-icon/></a>
@@ -47,9 +49,13 @@
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                             @if($domain->manages === 1)
-                                Us
+                                <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+                                  Us
+                                </span>
                             @else
-                                Client
+                                <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
+                                  Client
+                                </span>
                             @endif
                         </td>
                     </tr>
