cd8e2082242a2a61e84aa9898e5e7476eef18d96
jltkd <jl@turnkeydigital.com>
2022-02-25 15:34:11 -0600

    Refactor Domain List to display domain count

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Domain;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class ServerController extends Controller
 {
+    public $domainCount;
+
     public function show(Server $server)
     {
+        $domainCount = Domain::where('server_id', $server->id)->count();
         return view('servers.show', [
             'server' => $server
-        ]);
+        ], compact('domainCount'));
     }
+
 }
--- resources/views/servers/show.blade.php
+++ resources/views/servers/show.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ $server->name }}
+                {{ $server->name }} @if($domainCount)({{ $domainCount }})@endif
             </h2>
             <a href="http://{{ $server->ip_address }}:2086" target="_blank" class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Log In</a>
         </div>
